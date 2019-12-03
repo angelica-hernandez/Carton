8c8e582bab9d3c1a436a2c2b8e41c7fd2bf4e202
Janice <mjanice729@gmail.com>
2019-12-03 00:36:10 -0500

    Added Profile Page

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import blankprofile from "../assets/blankprofile.png";
+
+import "./Profile.scss";
+import ToBuyList from "./ToBuyList";
+
+class Profile extends Component {
+    state = {  }
+    constructor(props){
+        super(props);
+
+        this.state={
+            items:[]
+        };
+
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e) {
+        if(this._inputElement.value !== ""){
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) =>{
+                return{
+                    items: prevState.items.concat(newItem)
+                };
+            });
+        }
+        this._inputElement.value="";
+        console.log(this.state.items);
+        e.preventDefault();
+    }
+
+    deleteItem(key){
+        var filteredItems = this.state.items.filter(function(item){
+            return(item.key !== key)
+        });
+
+        this.setState({items: filteredItems});
+    }
+    
+    render() { 
+        return ( 
+            <div className="ProfilePage">
+                <div className="leftside">
+                    <h3>Your Profile</h3>
+                    <img src={blankprofile} className="ProfileImg"></img>
+                </div>
+
+                <div className="ItemsNeed">
+                    <h3>Items Need to Purchase:</h3>
+                    <div className="header">
+                        <form onSubmit={this.addItem}>
+                            <input ref={(a) => this._inputElement = a}
+                            placeholder="Enter Item"></input>
+                            <button type="submit">Add</button>
+                        </form>
+                    </div>
+                    <ToBuyList entries={this.state.items}
+                    delete={this.deleteItem}/>
+                </div>
+            </div>
+
+
+         );
+    }
+}
+
+export default Profile;
